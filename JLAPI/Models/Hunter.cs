72f05dd850db9c3e283080ef1595c6d21bfe5fde
Tore Mørkved <tore.morkved@gmail.com>
2017-09-06 21:48:06 +0200

    init

---
+++ JLAPI/Models/Hunter.cs
@@ -0,0 +1,12 @@
+﻿using JLAPI.Models.Base;
+
+namespace JLAPI.ModelsModels
+{
+    public class Hunter : EntityBase
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
---
+++ JLAPI/Models/SpeciesGroup.cs
@@ -0,0 +1,10 @@
+﻿using JLAPI.Models.Base;
+
+namespace JLAPI.ModelsModels
+{
+    public class SpeciesGroup
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
